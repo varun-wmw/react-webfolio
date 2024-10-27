d9142a8fb45a08d59e6605ca27d362b11dea0b48
Varun <varun@wandermewell.com>
2024-10-27 23:39:06 +0530

    update: project finished

--- electron/main.js
+++ electron/main.js
@@ -24,7 +24,7 @@ function createMainWindow() {
   mainWindow.loadURL(url);
   
   // Open DevTools for debugging
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 }
 
 
